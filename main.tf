2d8d0f200acdf58330c675ce2b67c5473f913693
takahiro1226 <8779928+ikuto8154@users.noreply.github.com>
2024-03-17 10:54:13 +0900

    初期tfファイルから、network.tfまで追加

---
+++ main.tf
@@ -0,0 +1,34 @@
+# ---------------------------------------------
+#  AWS 設定
+# ---------------------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "us-east-1"
+}
+
+# ---------------------------------------------
+# terraform 設定
+# ---------------------------------------------
+terraform {
+  required_version = ">=1.7.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+
+# ---------------------------------------------
+# Variables　input変数
+# ---------------------------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
